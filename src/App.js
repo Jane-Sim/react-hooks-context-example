c3026d38d1cab6efca34a4a858494a48705bfa59
심세영 <ssiox3@gmail.com>
2020-03-13 17:49:11 +0900

    filter기능으로 배열 항목 제거하기

--- src/App.js
+++ src/App.js
@@ -55,6 +55,12 @@ function App() {
     nextId.current += 1;
   };
 
+  const onRemove = id => {
+    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+    // = user.id 가 id 인 것을 제거함
+    setUsers(users.filter(user => user.id !== id));
+  };
+
   return (
     <>
       <CreateUser
@@ -63,7 +69,7 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
       />{' '}
-      <UserList users={users} />
+      <UserList users={users} onRemove={onRemove} />
     </>
   );
 }
