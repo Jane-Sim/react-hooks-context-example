faf52d1be15add2210b81284ce79a33f56540b65
심세영 <ssiox3@gmail.com>
2020-03-13 17:55:19 +0900

    spread와 map으로 di값이 일치하는 배열의 항목 수정하기

--- src/App.js
+++ src/App.js
@@ -24,16 +24,19 @@ function App() {
       id: 1,
       username: 'velopert',
       email: 'public.velopert@gmail.com',
+      active: true,
     },
     {
       id: 2,
       username: 'tester',
       email: 'tester@example.com',
+      active: false,
     },
     {
       id: 3,
       username: 'liz',
       email: 'liz@example.com',
+      active: false,
     },
   ]);
 
@@ -61,6 +64,14 @@ function App() {
     setUsers(users.filter(user => user.id !== id));
   };
 
+  const onToggle = id => {
+    setUsers(
+      users.map(user =>
+        user.id === id ? { ...user, active: !user.active } : user
+      )
+    );
+  };
+
   return (
     <>
       <CreateUser
@@ -69,7 +80,7 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
       />{' '}
-      <UserList users={users} onRemove={onRemove} />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
     </>
   );
 }
