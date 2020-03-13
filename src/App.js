5055fe3203a9c25ae1daa033a0b3f5a9a4df9db9
심세영 <ssiox3@gmail.com>
2020-03-13 15:45:20 +0900

    단축 평가 논리 계산법, props 값 설정을 생략하면 ={true}

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Wrapper from './Wrapper';
 function App() {
   return (
     <Wrapper>
-      <Hello name="react" color="red" />
+      <Hello name="react" color="red" isSpecial={true} />
       <Hello color="pink" />
     </Wrapper>
   );
