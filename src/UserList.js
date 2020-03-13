6ffae745bcec25605bffc11c4d3b0c37f6c1e17b
심세영 <ssiox3@gmail.com>
2020-03-13 18:40:47 +0900

    useEffect에 두 번쨰 파라미터 값을 빈배열로 넣었을 때, 마운트/언마운트 상태

--- src/UserList.js
+++ src/UserList.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function User({ user, onRemove, onToggle }) {
+  useEffect(() => {
+    console.log('컴포넌트가 화면에 나타남');
+    return () => {
+      console.log('컴포넌트가 화면에서 사라짐');
+    };
+  }, []);
   return (
     <div>
       <b
