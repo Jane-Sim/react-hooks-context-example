03b46880e00ab661358e9967b741b1334c7e3ba6
심세영 <ssiox3@gmail.com>
2020-03-13 17:27:15 +0900

    useRef를 이용한 컴포넌트 안에서 조회 및 수정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import UserList from './UserList';
 
 function App() {
-  return <UserList />;
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com',
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com',
+    },
+  ];
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    // 나중에 구현 할 배열에 항목 추가하는 로직
+    // ...
+
+    nextId.current += 1;
+  };
+
+  return <UserList users={users} />;
 }
 
 export default App;
