efc6f6f0a5cacad8a7071e9486598f65da73b842
심세영 <ssiox3@gmail.com>
2020-03-13 16:11:42 +0900

    Hooks의 useState로 상태값 관리하기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
+import Counter from './Counter';
 
 function App() {
-  return (
-    <Wrapper>
-      <Hello name="react" color="red" isSpecial={true} />
-      <Hello color="pink" />
-    </Wrapper>
-  );
+  return <Counter />;
 }
 
 export default App;
