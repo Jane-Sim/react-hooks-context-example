63bfa1b8a4751315b097f325e6ba33528bb4b88c
심세영 <ssiox3@gmail.com>
2020-03-13 17:46:59 +0900

    useRef로 배열을 효율적으로 추가하기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+
+  const { username, email } = inputs;
+
+  const onChange = e => {
+    const { name, value } = e.target;
+
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const [users, setUsers] = useState([
     {
       id: 1,
       username: 'velopert',
@@ -18,17 +35,37 @@ function App() {
       username: 'liz',
       email: 'liz@example.com',
     },
-  ];
+  ]);
 
+  // useRef() 를 사용 할 때 파라미터를 넣어주면, 이 값이 .current 값의 기본값이 됩니다.
+  // 그리고 이 값을 수정 할때에는 .current 값을 수정하면 되고 조회 할 때에는 .current 를 조회하면 됩니다.
   const nextId = useRef(4);
   const onCreate = () => {
-    // 나중에 구현 할 배열에 항목 추가하는 로직
-    // ...
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    setUsers([...users, user]);
 
+    setInputs({
+      username: '',
+      email: '',
+    });
     nextId.current += 1;
   };
 
-  return <UserList users={users} />;
+  return (
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />{' '}
+      <UserList users={users} />
+    </>
+  );
 }
 
 export default App;
