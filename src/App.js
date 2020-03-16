b7f4bb6c4f91fc9ea7c403bff6c0e9a6cff20fc5
심세영 <ssiox3@gmail.com>
2020-03-16 12:34:31 +0900

    Hook의 useMemo로 계산된 값 재사용하기.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useMemo } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
+
+function countActiveUsers(users) {
+  console.log('활성 사용자 수를 세는중...');
+  return users.filter(user => user.active).length;
+}
 
 function App() {
   const [inputs, setInputs] = useState({
@@ -72,6 +77,8 @@ function App() {
     );
   };
 
+  const count = useMemo(() => countActiveUsers(users), [users]);
+
   return (
     <>
       <CreateUser
@@ -81,6 +88,7 @@ function App() {
         onCreate={onCreate}
       />{' '}
       <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <div>활성사용자 수 : {count}</div>
     </>
   );
 }
--- src/UserList.js
+++ src/UserList.js
@@ -2,11 +2,8 @@ import React, { useEffect } from 'react';
 
 function User({ user, onRemove, onToggle }) {
   useEffect(() => {
-    console.log('컴포넌트가 화면에 나타남');
-    return () => {
-      console.log('컴포넌트가 화면에서 사라짐');
-    };
-  }, []);
+    console.log(user);
+  });
   return (
     <div>
       <b
